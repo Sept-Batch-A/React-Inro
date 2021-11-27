82211eac73aa546383b5a3885d311e0e347c1ee8
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-11-27 11:44:33 +0530

    React intro complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./style.css"
+import Button from "./Components/Button/Button.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = ()=>{
+    return(
+        <div>
+          <h1 style={{textAlign: "center"}}> Hey Everyone </h1>
+           <Button />
+        </div>
+    )
 }
 
-export default App;
+
+
+
+
+export default App
+
+
+
+// Named Export
+
+// export const SayHi = ()=>{
+//     return(
+//         <div>
+//           <h1 style={{textAlign: "center"}}> Hey Everyone </h1>
+//           <button className="button"> Apple</button>
+//           <button className="button"> Manago</button>
+//           <button className="button"> Gauva</button>
+//         </div>
+//     )
+// }
+
